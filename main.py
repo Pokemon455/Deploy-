51e3fc454b6899621413fadeec323ece9410cbec
Pokemon455 <arwazrozi@gmail.com>
2026-03-03 10:34:03 +0530

    update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from langchain_core.messages import HumanMessage, SystemMessage
 from fastapi import FastAPI
 
 llm = ChatOpenAI(
-    model="Llama-4-Maverick-17B-128E-Instruct",
+    model="Meta-Llama-3.1-8B-Instruct",
     api_key="d40720e9-a75f-46ba-84f5-6b6339732d9e",
     base_url="https://api.sambanova.ai/v1",
     temperature=0.9
